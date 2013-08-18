b62d4649d4b0ea5a0d261c8cd55ed646d3e780ca
Damian Krzeminski <pirxpilot@code42day.com>
2013-08-18 11:37:51 -0400

    Implement plumper in terms of grid coordinates

--- test/plumper.js
+++ test/plumper.js
@@ -5,18 +5,26 @@ var plumper = require('../lib/plumper');
 
 describe('plumper', function() {
 
-	it('should return nothing for invalid input', function() {
-		should.not.exist(plumper());
-		should.not.exist(plumper(4));
-		should.not.exist(plumper(4, []));
-	});
+  it('should return nothing for invalid input', function() {
+    should.not.exist(plumper());
+    should.not.exist(plumper(4));
+    should.not.exist(plumper(4, []));
+  });
 
-	it('should calculate simple polygon for a single point', function() {
-		var polygon = plumper(1, [[0, 0]]);
+  it('should calculate a single box for a single point', function() {
+    var boxes = plumper([[0, 0]], 1);
 
-		should.exist(polygon);
-		polygon.should.have.lengthOf(4);
-		polygon.should.be.eql([[0, 1], [1, 0], [0, -1], [-1, 0]]);
-	});
+    should.exist(boxes);
+    boxes.should.have.lengthOf(1);
+    boxes[0].should.have.lengthOf(2);
+    boxes[0].should.eql([[-1, -1], [1, 1]]);
+  });
 
+  it('should calculate a single box for a horizontal line', function() {
+     var boxes = plumper([[2, 3], [6, 3]], 1);
+
+     should.exist(boxes);
+     boxes.should.have.lengthOf(1);
+     boxes[0].should.be.eql([[1, 2], [7, 4]]);
+  });
 });
