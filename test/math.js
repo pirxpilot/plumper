6be479494bd0b7583ac40bcd780e59d96e0051e2
Damian Krzeminski <pirxpilot@code42day.com>
2013-08-18 23:17:42 -0400

    offset, scale, boxToOriginAndExtent added to math module

--- test/math.js
+++ test/math.js
@@ -39,3 +39,73 @@ describe('math.same', function() {
 		math.same([1.2, 5.2], [1, 5]).should.equal(false);
 	});
 });
+
+describe('math.offset', function() {
+
+	it('should substract if no d set', function() {
+		math.offset([1, 5], [2, 3]).should.eql([-1, 2]);
+		math.offset([1, 5], [2, 3], -1).should.eql([-1, 2]);
+	});
+
+	it('should add if d set to 1', function() {
+		math.offset([1, 5], [1.1, 5], 1).should.eql([2.1, 10]);
+		math.offset([1, 5], [-1, -3], 1).should.eql([0, 2]);
+	});
+});
+
+
+describe('math.scale', function() {
+
+	it('should convert to real coordinates', function() {
+		var scale = math.scale([-3, 7], 2);
+
+		scale.toReal([0, 0]).should.eql([-3, 7]);
+		scale.toReal([1, 2]).should.eql([-1, 11]);
+
+		scale.toReal([
+			[0, 0],
+			[1, 2],
+			[5, 7]
+		]).should.eql([
+			[-3, 7],
+			[-1, 11],
+			[7, 21]
+		]);
+	});
+
+	it('should convert to grid coordinates', function() {
+		var scale = math.scale([-3, 7], 2);
+
+		scale.toGrid([-3, 7]).should.eql([0, 0]);
+		scale.toGrid([-2.5, 8.4]).should.eql([0, 0]);
+		scale.toGrid([7.3, 22.1]).should.eql([5, 7]);
+
+		scale.toGrid([
+			[-3, 7],
+			[-2.5, 8.4],
+			[7.3, 22.1]			
+		]).should.eql([
+			[0, 0],
+			[0, 0],
+			[5, 7]		
+		]);
+	});
+});
+
+describe('math.boxToOriginAndExtent', function() {
+  it('should calculate origin and extent', function() {
+    var oae;
+
+    oae = math.boxToOriginAndExtent([[0, 0], [2, 6.4]], 3);
+    oae.should.have.property('origin').eql([-5, -5.8]);
+    oae.should.have.property('extent').eql([4, 6]);
+
+    oae = math.boxToOriginAndExtent([[-1, 0], [2, 30]], 3);
+    oae.origin.should.eql([-5.5, -6]);
+    oae.extent.should.eql([4, 14]);
+
+    oae = math.boxToOriginAndExtent([[2, 3], [6, 3]], 1);
+    oae.origin.should.eql([0, 1]);
+    oae.extent.should.eql([8, 4]);
+  });
+});
