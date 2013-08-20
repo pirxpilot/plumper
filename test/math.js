f07dfeff9c1a351f6b71ce1cb06588db70dea5ff
Damian Krzeminski <pirxpilot@code42day.com>
2013-08-20 11:54:05 -0400

    Use spaces for indentation

--- test/math.js
+++ test/math.js
@@ -5,91 +5,91 @@ var math = require('../lib/math');
 
 describe('math.div', function() {
 
-	it('should perform integral division', function() {
-		math.div(5, 2).should.eql(2);
-		math.div(10.1, 2).should.eql(5);
-		math.div(6, 2.3).should.eql(2);
-		math.div(7.4, 1.1).should.eql(6);
-	});
+  it('should perform integral division', function() {
+    math.div(5, 2).should.eql(2);
+    math.div(10.1, 2).should.eql(5);
+    math.div(6, 2.3).should.eql(2);
+    math.div(7.4, 1.1).should.eql(6);
+  });
 
 });
 
 describe('math.sign', function() {
 
-	it('should return "1" for positive numbers', function() {
-		math.sign(5).should.eql(1);
-		math.sign(5.3).should.eql(1);
-	});
+  it('should return "1" for positive numbers', function() {
+    math.sign(5).should.eql(1);
+    math.sign(5.3).should.eql(1);
+  });
 
-	it('should return "-1" for negative numbers', function() {
-		math.sign(-5).should.eql(-1);
-		math.sign(-5.3).should.eql(-1);
-	});
+  it('should return "-1" for negative numbers', function() {
+    math.sign(-5).should.eql(-1);
+    math.sign(-5.3).should.eql(-1);
+  });
 });
 
 describe('math.same', function() {
 
-	it('should be true if points are equal', function() {
-		math.same([1, 5], [1, 5]).should.equal(true);
-	});
+  it('should be true if points are equal', function() {
+    math.same([1, 5], [1, 5]).should.equal(true);
+  });
 
-	it('should be false if points are different', function() {
-		math.same([1, 5], [1.1, 5]).should.equal(false);
-		math.same([1, 5.1], [1, 5]).should.equal(false);
-		math.same([1.2, 5.2], [1, 5]).should.equal(false);
-	});
+  it('should be false if points are different', function() {
+    math.same([1, 5], [1.1, 5]).should.equal(false);
+    math.same([1, 5.1], [1, 5]).should.equal(false);
+    math.same([1.2, 5.2], [1, 5]).should.equal(false);
+  });
 });
 
 describe('math.offset', function() {
 
-	it('should substract if no d set', function() {
-		math.offset([1, 5], [2, 3]).should.eql([-1, 2]);
-		math.offset([1, 5], [2, 3], -1).should.eql([-1, 2]);
-	});
+  it('should substract if no d set', function() {
+    math.offset([1, 5], [2, 3]).should.eql([-1, 2]);
+    math.offset([1, 5], [2, 3], -1).should.eql([-1, 2]);
+  });
 
-	it('should add if d set to 1', function() {
-		math.offset([1, 5], [1.1, 5], 1).should.eql([2.1, 10]);
-		math.offset([1, 5], [-1, -3], 1).should.eql([0, 2]);
-	});
+  it('should add if d set to 1', function() {
+    math.offset([1, 5], [1.1, 5], 1).should.eql([2.1, 10]);
+    math.offset([1, 5], [-1, -3], 1).should.eql([0, 2]);
+  });
 });
 
 
 describe('math.scale', function() {
 
-	it('should convert to real coordinates', function() {
-		var scale = math.scale([-3, 7], 2);
+  it('should convert to real coordinates', function() {
+    var scale = math.scale([-3, 7], 2);
 
-		scale.toReal([0, 0]).should.eql([-3, 7]);
-		scale.toReal([1, 2]).should.eql([-1, 11]);
+    scale.toReal([0, 0]).should.eql([-3, 7]);
+    scale.toReal([1, 2]).should.eql([-1, 11]);
 
-		scale.toReal([
-			[0, 0],
-			[1, 2],
-			[5, 7]
-		]).should.eql([
-			[-3, 7],
-			[-1, 11],
-			[7, 21]
-		]);
-	});
+    scale.toReal([
+      [0, 0],
+      [1, 2],
+      [5, 7]
+    ]).should.eql([
+      [-3, 7],
+      [-1, 11],
+      [7, 21]
+    ]);
+  });
 
-	it('should convert to grid coordinates', function() {
-		var scale = math.scale([-3, 7], 2);
+  it('should convert to grid coordinates', function() {
+    var scale = math.scale([-3, 7], 2);
 
-		scale.toGrid([-3, 7]).should.eql([0, 0]);
-		scale.toGrid([-2.5, 8.4]).should.eql([0, 0]);
-		scale.toGrid([7.3, 22.1]).should.eql([5, 7]);
+    scale.toGrid([-3, 7]).should.eql([0, 0]);
+    scale.toGrid([-2.5, 8.4]).should.eql([0, 0]);
+    scale.toGrid([7.3, 22.1]).should.eql([5, 7]);
 
-		scale.toGrid([
-			[-3, 7],
-			[-2.5, 8.4],
-			[7.3, 22.1]			
-		]).should.eql([
-			[0, 0],
-			[0, 0],
-			[5, 7]		
-		]);
-	});
+    scale.toGrid([
+      [-3, 7],
+      [-2.5, 8.4],
+      [7.3, 22.1]     
+    ]).should.eql([
+      [0, 0],
+      [0, 0],
+      [5, 7]    
+    ]);
+  });
 });
 
 describe('math.boxToOriginAndExtent', function() {
